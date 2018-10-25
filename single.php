76c7ba091b760357c4c6a4e3be1a34060e5b8a7a
Alberto Bezerra <albertofbezerra@gmail.com>
2018-10-25 00:13:40 -0300

    Alberto Bezerra
    Alterações na single inserção de publicidade

--- single.php
+++ single.php
@@ -11,6 +11,8 @@
 
 				<div class="col-12 col-md-8">
 
+					<div class="loc-img"><?php the_post_thumbnail(false, array('class'=>"thumb-principal")); ?></div>
+
 					<div class="titulo-do-post"><h3><?php the_title(); ?></h3></div>	
 
 						<?php the_content(); ?>
@@ -19,7 +21,32 @@
 
 				</div>
 
-				<div class="col-6 col-md-4 espacolateral">Oremos</div>			
+				<div class="col-6 col-md-4">
+
+					<div class="autor">Informações do Autor</div>
+
+					<div class="maisvistos">Mais vistos</div>
+
+					<div class="ultimaspublicacoes">Últimas publicações</div>
+
+					<div class="publicidade">Publicidade
+						
+						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+							<ins class="adsbygoogle"
+							     style="display:block"
+							     data-ad-format="fluid"
+							     data-ad-layout-key="-73+ed+2i-1n-4w"
+							     data-ad-client="ca-pub-7979689703488774"
+							     data-ad-slot="5425200500"></ins>
+							<script>
+							     (adsbygoogle = window.adsbygoogle || []).push({});
+						</script>
+
+					</div>
+
+
+
+				</div>		
 		
 				
 	
