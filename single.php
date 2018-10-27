58048aae98489fc1b22e1cee594baadacb354059
Alberto Bezerra <albertofbezerra@gmail.com>
2018-10-27 17:26:33 -0300

    Alberto Bezerra
    Confeccao da area dos posts mais vistos

--- single.php
+++ single.php
@@ -30,11 +30,52 @@
 
 					</div>
 
-					<div class="maisvistos">Mais vistos
+					<div class="maisvistos">
 
-						<?php if (function_exists('get_most_viewed')): ?>
-            			<?php get_most_viewed('post',5); ?> 
-          				<?php endif; ?>
+						<p class="mais-visto-chamada">Post's Mais vistos</p>
+
+						<?php
+							$nova_consulta = new WP_Query( 
+							    array(
+							        'posts_per_page'      => 5,                 // Máximo de 5 artigos
+							        'no_found_rows'       => true,              // Não conta linhas
+							        'post_status'         => 'publish',         // Somente posts publicados
+							        'ignore_sticky_posts' => true,              // Ignora posts fixos
+							        'orderby'             => 'meta_value_num',  // Ordena pelo valor da post meta
+							        'meta_key'            => 'tp_post_counter', // A nossa post meta
+							        'order'               => 'DESC'             // Ordem decrescente
+							    )
+							);
+						?>
+
+						<?php if ( $nova_consulta->have_posts() ): ?>
+       					<?php while ( $nova_consulta->have_posts() ): ?>
+        
+			            <?php $nova_consulta->the_post(); ?>
+			            <?php $tp_post_counter = get_post_meta( $post->ID, 'tp_post_counter', true );?>
+            
+            			<div class="mais-visto clearfix">
+                
+			                <?php if( has_post_thumbnail() ): ?>
+			                    <div class="mais-visto-thumbnail">
+			                        <a href="<?php the_permalink(); ?>">
+			                            <?php the_post_thumbnail('mais-visto-thumbnail'); ?>
+			                        </a>
+			                    </div> <!-- .mais-visto-thumbnail -->
+			                <?php endif; // has_post_thumbnail ?>
+	                
+			                <h4 class="mais-visto-titulo">
+			                    <a href="<?php the_permalink(); ?>">
+			                        <?php the_title();?>
+			                    </a>
+			                </h4><br> <!-- .mais-visto-titulo -->
+            
+           				</div> <!-- .mais-visto -->
+            
+				        	<?php endwhile; ?>
+					   	 <?php endif; // have_posts ?>
+
+				    	<?php wp_reset_postdata(); ?>
 
 					</div>
 
